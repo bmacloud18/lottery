a06b4f09879e8fd6e5881a1edc15f5b94f5bebd0
Benjamin Abbott <bmabbott@ncsu.edu>
2024-08-26 18:22:36 -0400

    fixed color bug

--- frontend/app/components/lottoTile.tsx
+++ frontend/app/components/lottoTile.tsx
@@ -8,12 +8,12 @@ export default function LottoTile({
     item:Item,
     remove: any
 }) {
-    const color = colors[item.id].name;
+    const color = colors[item.id].hex
     
     return (
         <div key={item.id} className="mb-1 border-solid border-2 border-black rounded-xl w-full p-2 overflow-hidden flex flex-row gap-2 justify-between">
-            <div className={`size-6 border-solid border bg-${color}`}></div>
-            <span className="text-center text-base">{item.name}</span>
+            <div style={{ backgroundColor: color }} className={`size-6 border-solid border`}></div>
+            <span className={`text-center text-base`}>{item.name}</span>
             <button onClick={remove}>
                 <img src="/deletepng.png" alt="delete" className="size-2"/>
             </button>
