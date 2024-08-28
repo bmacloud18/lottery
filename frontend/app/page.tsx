9e81148180b4ac97ac2e96eb15376fa9c7720f59
Benjamin Abbott <bmabbott@ncsu.edu>
2024-08-27 23:38:38 -0400

    changed 'replacement' factors to 'removed'/'removal'. added comments to animation function and all other functions on main page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,7 +11,7 @@ import sampleColors from "@/app/util/colors";
 export default function Home() {
 
   const [items, setItems] = useState<Item[]>([]);
-  const [replaced, setReplaced] = useState<Item[]>([]);
+  const [removed, setRemoved] = useState<Item[]>([]);
   const [id, setId] = useState<number>(0);
   const [popup, setPopup] = useState<boolean>(false);
 
@@ -24,19 +24,23 @@ export default function Home() {
   const [disabled, setDisabled] = useState(false);
   const timeout = useRef<NodeJS.Timeout | null>(null);
 
+
+  //resets the page to default values of 3 items, no removal
+  //stops any active spin and resets colors array
   const restart = () => {
     const sampleItems = samples
 
     setColors(sampleColors);
     setItems(sampleItems);
     setId(sampleItems.length - 1);
-    setReplaced([]);
+    setRemoved([]);
     setAnimation({});
     if (timeout.current) {
       clearTimeout(timeout.current);
     }
   }
 
+  //get a color from the colors array in util or creates a random hex value if all colors have been taken
   const getColor = () => {
     let col;
     if (colors.length > 0) {
@@ -53,6 +57,7 @@ export default function Home() {
     return col;
   }
 
+  //add an item to the list of items for the wheel to choose from
   const addItem = (name: string) => {
     if (items.length < 36) {
       const randomColor = getColor();
@@ -67,10 +72,13 @@ export default function Home() {
     }
   }
 
-  const addReplaced = (item: Item) => {
-    setReplaced([...replaced, item])
+  //add an item to the list of removed items
+  const addRemoved = (item: Item) => {
+    setRemoved([...removed, item])
   }
 
+  //removes a selected item and adds their color back to the list of colors to choose from for new items
+  //does not call add item to the removed list by default, done separately by the above function
   const removeItem = (id: number) => {
     if (items.length > 1) {
       const newArray = items.filter((item, idx) => idx !== id);
@@ -80,6 +88,7 @@ export default function Home() {
     }
   }
 
+  //changes removal value
   const changeRemoval = () => {
     if (removal) {
       setRemoval(false);
@@ -89,42 +98,41 @@ export default function Home() {
     }
   }
 
+  //component for the removal button, green when removal is on, red when off (defaults off)
   const removalButton = removal ? (
     <Button onClick={changeRemoval} text={'Removal On'} disabled={false}></Button>
   ) : (
     <Button onClick={changeRemoval} text={'Removal Off'} disabled={false}></Button>
   )
 
+  //set to default wheel upon load
   useEffect(() => {
     if (items.length === 0) {
       restart();
     }
   }, [items]);
 
+
+  //most important piece, calculates spin and determines winner following spin
   const startAnimation = () => {
+    //disable the spin button so spins/calculations/results don't stack
     setDisabled(true);
+
+    //randomize spin
     const angle = Math.random() * 360;
-    const newPosition = position + 360 * 10 + angle;
+    const offset = Math.random() * 8 + 4
+    const newPosition = position + 360 * offset + angle;
     const dn = Math.floor(5 + Math.random() * 100 % 5)
     const duration = `${dn}s`;
 
 
-    // Calculate the final index after the spin completes
-    timeout.current = setTimeout(() => {
-      setSelected(items[idx].name);
-      if (removal) {
-        removeItem(idx);
-        addReplaced(items[idx]);
-      }
-      setDisabled(false);
-    }, dn * 1000); // Wait for the animation to complete
-
+    //set animation of the wheel component to calculated spin
     setAnimation({
       transform: `rotate(${newPosition}deg)`,
       transition: `transform ${duration} ease-out`,
     });
 
-    console.log(position, newPosition);
+    //calculate new position after spin and find corresponding idx
     const n = items.length;
     const arclength = 360 / n;
     const degree = newPosition % 360;
@@ -136,6 +144,17 @@ export default function Home() {
       }
     }
 
+    //make sure calculations and results don't take effect until after spin
+    timeout.current = setTimeout(() => {
+      setSelected(items[idx].name);
+      if (removal) {
+        removeItem(idx);
+        addRemoved(items[idx]);
+      }
+      setDisabled(false);
+    }, dn * 1000);
+
+    //save the new position for next calculation
     setPosition(newPosition);
   }
 
@@ -156,7 +175,7 @@ export default function Home() {
           <div className="absolute top-0 left-1/2 h-1/5 w-0.5 bg-yellow transform -translate-x-1/2"></div>
         </div>
 
-        <Grid removeItem={(undefined)} items={replaced}></Grid>
+        <Grid removeItem={(undefined)} items={removed}></Grid>
 
       </div>
 
